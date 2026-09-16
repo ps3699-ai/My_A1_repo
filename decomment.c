182755773af4fecf65fc2130724bc1069fd312cf
Peter Strecansky <ps3699@princeton.edu>
2026-09-16 12:50:39 -0400

    DONE: functions,main nearly done must add comments and test and debub git add decomment.c ! used cloude for help with error comment prompt: if (state == STATE_COMMENT || state == STATE_MAYBE_END_COMMENT) this is my if how can I print the result error message in document wich i make communication to > , is printf f eneugh or shoul I use fprintf

--- decomment.c
+++ decomment.c
@@ -126,4 +126,53 @@ enum Statetype handleInCharEscape(int c)
         return state;
 }
 
+int main(void)
+{
+	int c;
+	enum Statetype prevState;
+	int lane = 1; 
+	int commentStartLine = 0;
+	enum Statetype state = STATE_TEXT;
+	while ((c = getchar())!= EOF)
+		prevState = state;
+	{
+			switch (state) {
+        	case STATE_TEXT:
+                	state = handleInText(c);
+                	break;
+        	case STATE_MAYBE_COMMENT:
+                	state = maybeInComment(c);
+                	break;
+        	case STATE_COMMENT:
+                	state = handleComment(c);
+                	break;
+        	case STATE_MAYBE_END_COMMENT:
+                	state = handleMaybeEndComment(c);
+                	break;
+        	case STATE_STRING:
+        	        state = handleInString(c);
+                	break;
+       	 	case STATE_STRING_ESCAPE:
+                	state = handleInStringEscape(c);
+                	break;
+        	case STATE_CHAR:
+                	state = handleInChar(c);
+                	break;
+        	case STATE_CHAR_ESCAPE:
+                	state = handleInCharEscape(c);
+                	break;
+		}
+		}
+		if(prevState == STATE_MAYBE_COMMENT && state == STATE_COMMENT){
+			commentStartline = line;
+		}
+		if(c=='\n'){line ++}
+		if (state == STATE_COMMENT || state == STATE_MAYBE_END_COMMENT) {
+        		fprintf(stderr, "Error: line %d: unterminated comment\n", commentStartLine);
+        		return EXIT_FAILURE;
+		} else {
+       		 return EXIT_SUCCESS;
+		}	
+	}
 
+}
